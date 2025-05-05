c81ed15d6093302083673ecfd236c03f99e5be7c
yesuri-tihara <yesuriayupala@gmail.com>
2025-05-05 10:44:34 +0530

    handling an error in the code

--- streamlitapp.py
+++ streamlitapp.py
@@ -11,41 +11,7 @@ st.set_page_config(
     page_icon="🏙️",
     layout="wide"
 )
-
-st.markdown("""
-<style>
-@keyframes border-runner {
-  0%   { top: 0; left: 0; }
-  25%  { top: 0; left: 100%; transform: translateX(-100%); }
-  50%  { top: 100%; left: 100%; transform: translate(-100%, -100%); }
-  75%  { top: 100%; left: 0; transform: translateY(-100%); }
-  100% { top: 0; left: 0; }
-}
-
-div.block-container {
-    position: relative;
-    margin-top: 65px;
-    margin-bottom:10px;
-    border: 2px solid #39FF14;  
-    border-radius: 8px;        
-    padding: 10px;              
-    box-shadow: 0 0 15px rgba(57, 255, 20, 0.3);
-}
-
-
-div.block-container::before {
-    content: "";
-    width: 12px;
-    height: 12px;
-    background-color: #39FF14;
-    border-radius: 50%;
-    position: absolute;
-    animation: border-runner 12s linear infinite;
-    box-shadow: 0 0 10px 4px rgba(57, 255, 20, 0.6);
-    z-index: 999;
-}
-</style>
-""", unsafe_allow_html=True)
+  
 # Load data
 @st.cache_data
 def load_data():
